655e89f5bf89cdd9b1b888bcf4e47f412ef2eb82
Tatsuya Nakamura <al1.1ion.nkmr@gmail.com>
2016-08-24 20:03:06 +0900

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+__author__ = 'Tatsuya Nakamura'
+
+setup(
+    name='episode_mining',
+    version='0.1',
+    description='An inplementation of sequential patterm mining method',
+    author='Tatsuya Nakamura',
+    author_email='nkmrtty.com@gmail.com',
+    url='https://github.com/nkmrtty/episode_mining',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2 :: Only',
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    keywords=['episode mining', 'pattern mining'],
+    license='MIT License',
+    install_requires=[],
+)
